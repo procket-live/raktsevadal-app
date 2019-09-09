36c04d3406529683401697be8ad09e85ca5530ca
Himanshu Kushwah <himanshu.kushwaha@drivezy.com>
2019-09-09 21:15:13 +0530

    network state added

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, { Fragment, PureComponent } from 'react';
 import SplashScreen from 'react-native-splash-screen';
+import NetworkState, { Settings } from 'react-native-network-state'
 import {
   View,
   Text,
@@ -20,9 +21,15 @@ class App extends PureComponent {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
-        <Text>App</Text>
-      </View>
+      <Fragment>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
+          <Text>App</Text>
+        </View>
+        <NetworkState
+          style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 }}
+        />
+      </Fragment>
+
     )
   }
 }
