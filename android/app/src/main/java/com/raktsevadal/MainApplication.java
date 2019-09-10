1c1e4f1edd909b88856ecd159de907bde4d9c387
Himanshu Kushwah <himanshu.kushwaha@drivezy.com>
2019-09-10 15:46:48 +0530

    storage added

--- android/app/src/main/java/com/raktsevadal/MainApplication.java
+++ android/app/src/main/java/com/raktsevadal/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.storage.RNFirebaseStoragePackage;
 
 import java.util.List;
 
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       packages.add(new RNFirebaseMessagingPackage());
+      packages.add(new RNFirebaseStoragePackage());
       return packages;
     }
 
