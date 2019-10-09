7d323f9d0564e2217e86355cabc188fa72c90bcf
Himanshu Kushwah <himanshu.kushwaha@drivezy.com>
2019-10-09 13:46:59 +0530

    dsf

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,4 @@
 node ./node_modules/@mauron85/react-native-background-geolocation/scripts/postlink.js
 sed -i '' -e 's/sourceCompatibility JavaVersion.VERSION_1_6//g' ./node_modules/@mauron85/react-native-background-geolocation/android/common/build.gradle
 sed -i '' -e 's/targetCompatibility JavaVersion.VERSION_1_6//g' ./node_modules/@mauron85/react-native-background-geolocation/android/common/build.gradle
+sed -i '' -e "41s/\${safeExtGet('googlePlayServicesVersion', '17.0.0')}/17.0.0/g" ./node_modules/react-native-android-location-enabler/android/build.gradle
